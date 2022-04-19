5b448be1ccac9e861315aac1afbe20b2f42a7756
Edon Morina <edonmorinaaa@hotmail.com>
2022-04-19 11:58:33 +0200

    Added a slider in home page

--- frontend/src/Pages/Home/Components/HomePageSecondSection.jsx
+++ frontend/src/Pages/Home/Components/HomePageSecondSection.jsx
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../scss/main.scss';
-import ImageSlider from '../Components/ImageSlider';
-import { SliderData } from './SliderData';
+import { BsArrowLeftShort } from 'react-icons/bs';
+import { BsArrowRightShort } from 'react-icons/bs';
+
+import firstImg from '../assets/slide_images/1.jpg';
+import secondImg from '../assets/slide_images/2.jpg';
+import thirdImg from '../assets/slide_images/3.jpg';
+
 
 function HomePageSecondSection() {
 
+    const [imgIndex, setImgIndex] = useState(0);
+
+    const prevImg = () => {
+        if(imgIndex === 0) {
+            setImgIndex(slideImages.length - 1);
+        } else {
+            setImgIndex(imgIndex - 1);
+        }
+
+    }
+
+    const nextImg = () => {
+        if(imgIndex === (slideImages.length - 1)) {
+            setImgIndex(0);
+        } else {
+            setImgIndex(imgIndex + 1);
+        }
+    }
+
+    const slideImages = [
+        {
+            url: firstImg,
+            caption: 'Slide 1',
+            title: 'Philippines',
+        },
+        {
+            url: secondImg,
+            caption: 'Slide 2',
+            title: 'Greece',
+        },
+        {
+            url: thirdImg,
+            caption: 'Slide 3',
+            title: 'Austria',
+        }
+    ]
+
     return (
-        <div className="home-ss">
-        </div>
+        <section className='home-ss'>
+            <div className="img-c"><img src={slideImages[imgIndex].url} alt="random" /></div>
+            <button className='prev-img' onClick={() => prevImg()}><BsArrowLeftShort /></button>
+            <button className='next-img' onClick={() => nextImg()}><BsArrowRightShort /></button>
+            <h1>{slideImages[imgIndex].title}</h1>
+        </section>
     )
 }
 
